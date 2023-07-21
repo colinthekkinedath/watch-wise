1da5cefb97f804b36e838c9dfc88b9e1b38e1cb8
colinthekkinedath <colinthekkinedath@gmail.com>
2023-07-20 19:54:54 -0700

    initial setup

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+        <a>watch wise</a>
+    </header>
+  )
+}
+
+export default Header
